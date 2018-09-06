e3453f5e6ca9af46628b1e1d7e039769c0b8a5f7
Jimmy <jeremy.simonklein@gmail.com>
2018-09-06 22:56:56 +0200

    Installed react and react-dom Displayed from main.js

--- client/main.js
+++ client/main.js
@@ -1,3 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+
 Meteor.startup(() => {
     console.log('CLIENT : init')
+    render(
+        <p>izejfoizjef</p>, document.getElementById('root')
+    )
 })
