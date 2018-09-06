1f571fc47fded28866e65147a52bf586230fb1a8
Jimmy <jeremy.simonklein@gmail.com>
2018-09-06 23:08:07 +0200

    Added UserPage

---
+++ imports/pages/UserPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export default class UserPage extends Component {
+    state = {
+        
+    }
+
+    render(){
+        return(
+            <p>USER</p>
+        )
+    }
+}
--- imports/startup/App.js
+++ imports/startup/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
-import Landing from '/imports/pages/Landing'
+import { Landing, UserPage } from '/imports/pages'
 
 const App = () => {
     return (
         <Router>
             <Switch>
                 <Route path="/" exact component={Landing} />
+                <Route path="/user" exact component={UserPage} />
             </Switch>
         </Router>
     )
